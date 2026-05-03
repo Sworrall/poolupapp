8310fafbf463c05f71b0879e854777e8ecc17b7a
stephen <stephenlworrall@gmail.com>
2026-05-03 13:47:31 +0100

    first run Doubles working TO firebase

--- src/main/java/com/stephen/Main.java
+++ src/main/java/com/stephen/Main.java
@@ -11,19 +11,26 @@ public class Main {
         log.info("PoolManager application starting...");
         FirebaseConfig.initialise();
 
-        // Create and save a player
+        // Create and save Players
         Player player1 = new Player("Dan", "Worrall");
         Player player2 = new Player("John", "Smith");
         Player_Repository playerRepo = new Player_Repository();
         playerRepo.savePlayer(player1);
         playerRepo.savePlayer(player2);
 
-        // Create and save a team
+        // Create and save a Team
         Team team = new Team("The A Team");
         team.addPlayer(player1);
         team.addPlayer(player2);
         Team_Repository teamRepo = new Team_Repository();
         teamRepo.saveTeam(team);
+
+        // create and save Doubles Team
+        Doubles doublesTeam = new Doubles("Double the Trouble");
+        doublesTeam.addPlayer(player1);
+        doublesTeam.addPlayer(player2);
+        Doubles_Repository doublesRepo = new Doubles_Repository();
+        doublesRepo.saveDoublesTeam(doublesTeam);
 
         log.info("Done!");
     }
